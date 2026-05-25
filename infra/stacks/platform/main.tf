356c80f4b549287bfffb0111b3148760d315001c
Aram Abrahamyan <aram.abrahamyan@powerdata.de>
2026-05-25 23:24:16 +0400

    Add Terraform validation workflow

--- infra/stacks/platform/main.tf
+++ infra/stacks/platform/main.tf
@@ -8,7 +8,7 @@ module "vpc" {
   public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
 
-   enable_nat_gateway = var.enable_nat_gateway
+  enable_nat_gateway = var.enable_nat_gateway
 }
 
 module "eks" {
