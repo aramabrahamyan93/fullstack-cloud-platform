d00a34e21b2168cfc3996c15ee46be1ca4e5a5e0
Aram Abrahamyan <aram.abrahamyan@powerdata.de>
2026-05-26 00:16:00 +0400

    Allow Terraform CI to refresh AWS resources

--- infra/modules/iam-github-oidc/main.tf
+++ infra/modules/iam-github-oidc/main.tf
@@ -121,3 +121,8 @@ resource "aws_iam_role_policy" "terraform_state" {
     ]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "ci_read_only" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
